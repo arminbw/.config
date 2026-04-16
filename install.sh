44ec3b7f36d9fd0f54b892754bb33f5545efde06
Armin Wagner <547849+arminbw@users.noreply.github.com>
2026-04-16 16:08:04 +0200

    added some notes for a linux install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "Installing some basic terminal tools…"
 brew install fd
+#apt install starship apt-get install fd-find
 echo "Installing NeoVim…"
 brew install neovim
 echo "Installing vim plugin manager (vim-plug) and color scheme…"
@@ -13,8 +14,9 @@ brew tap homebrew/cask-fonts
 brew cask install font-hack
 echo "Installing Kitty terminal emulator"
 brew install kitty
-echo "Installing fzf and rg"
+echo "Installing fzf and rg and fd"
 brew install fzf
 brew install rg
+# apt install ripgrep
 echo "Please put 'source .config/zsh/.zshrc' into your .zshrc"
 echo "Please start vim and do :PlugInstall so vim-plug does its thing"
